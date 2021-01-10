24ba6b6f5d167390e23460bd9be59f4492854da8
zh <1535455969@qq.com>
2021-01-10 21:56:18 +0800

    整合jpa

--- springcloud/service1/src/main/java/zh/dao/jpa/UserJpa.java
+++
@@ -1,9 +0,0 @@
-package zh.dao.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import zh.entity.User;
-
-@Repository
-public interface UserJpa extends JpaRepository<User, Integer> {
-}
--- springcloud/service1/src/main/java/zh/entity/User.java
+++ springcloud/service1/src/main/java/zh/entity/User.java
@@ -1,18 +1,16 @@
 package zh.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.Id;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "user")
 public class User {
+    @Id
     private String username;
     private String password;
 }
